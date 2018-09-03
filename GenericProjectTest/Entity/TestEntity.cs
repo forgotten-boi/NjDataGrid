57534c19a70187cdf2584cc385c750ba7ce0b62a
Nish <nishjha4u@live.com>
2018-09-03 18:07:18 +0545

    Added new console project to use NjGrid as library

---
+++ GenericProjectTest/Entity/TestEntity.cs
@@ -0,0 +1,14 @@
+﻿using NjGrid.Entity.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericProjectTest.Entity
+{
+    public class TestEntity : BaseEntity<int>
+    {
+        public string TestString { get; set;}
+        public DateTime TestDate { get; set; }
+        public int TestInt { get; set; }
+    }
+}
---
+++ GenericProjectTest/Service/TestService.cs
@@ -0,0 +1,21 @@
+﻿using GenericProjectTest.Entity;
+using NjGrid.Repository;
+using NjGrid.Repository.Repositories;
+using NjGrid.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.Repositories;
+
+namespace GenericProjectTest.Service
+{
+    public class TestService : ApplicationService<TestEntity, int>, ITestService
+    {
+        public TestService(ITestRepository testRepository) : base(testRepository)
+        {
+
+        }
+
+
+    }
+}
